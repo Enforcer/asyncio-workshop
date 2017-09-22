dd312491b67924c57eb995ba5e7b25fcda90cffc
Sebastian Buczyński <nnplaya@gmail.com>
2017-09-22 21:51:23 +0200

    Implementacja pierwszej czesci

--- projekt/app.py
+++ projekt/app.py
@@ -1,5 +1,9 @@
 import asyncio
+import json
+import logging
 import time
+from collections import deque
+from contextlib import contextmanager
 
 from aiohttp import (
     web,
@@ -7,27 +11,66 @@ from aiohttp import (
 )
 
 
+chat_logger = logging.getLogger('chat_logger')
+handler = logging.StreamHandler()
+handler.setFormatter(
+    logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s')
+)
+chat_logger.addHandler(handler)
+chat_logger.setLevel(level=logging.INFO)
+
+
+@contextmanager
+def chat_presence(app, nickname, ws):
+    app['chat_members'][nickname] = ws
+    chat_logger.info('%s joins the chat', nickname)
+    yield
+    chat_logger.info('%s leaves the chat', nickname)
+    del app['chat_members'][nickname]
+
+
+def get_messages_from_last_10_minutes(app):
+    ten_minutes_ago = time.time() - 60 * 10
+    app['messages_archive'] = [
+        message for message in app['messages_archive']
+        if message['timestamp'] >= ten_minutes_ago
+    ]
+    return app['messages_archive']
+
+
 async def websocket_handler(request):
+    nickname = request.GET['nickname']
     ws = web.WebSocketResponse()
     await ws.prepare(request)
 
-    # ws.close() - closes connection
-    # ws.send_str(str) - sends a str
+    members_dict = request.app['chat_members']
 
-    async for msg in ws:
-        # msg.type - type of a message. See WSMsgType
-        # msg.data - data received
+    # sending old messages
+    archival_messages = get_messages_from_last_10_minutes(request.app)
+    for message in archival_messages:
+        await ws.send_json(message)
 
-        if msg.type == WSMsgType.TEXT:
-            await ws.send_json(
-                {
-                    'message': 'response from the server',
-                    'from': 'anonymous',
+    with chat_presence(request.app, nickname, ws):
+
+        async for msg in ws:
+
+            if msg.type == WSMsgType.TEXT:
+                decoded_data = msg.json()
+                message = {
+                    'message': decoded_data['message'],
+                    'from': nickname,
                     'timestamp': time.time()
                 }
-            )
-        elif msg.type == WSMsgType.ERROR:
-            print('ws connection closed with exception %s' % ws.exception())
+                request.app['messages_archive'].append(message)
+                coros = [
+                    member_ws.send_json(message)
+                    for member_ws in members_dict.values()
+                ]
+                # return_exceptions silents exceptions because they are returned
+                # in a list, not thrown
+                await asyncio.gather(*coros, return_exceptions=True)
+            elif msg.type == WSMsgType.ERROR:
+                chat_logger.warning('ws connection closed with exception %s', ws.exception())
 
     return ws
 
@@ -45,7 +88,7 @@ async def reconnecting_websocket(request):
 
 
 async def members(request):
-    return web.json_response([])
+    return web.json_response(list(request.app['chat_members'].keys()))
 
 
 async def rooms(request):
@@ -62,9 +105,32 @@ def create_app(loop=None):
     app.router.add_get('/style.css', css)
     app.router.add_get('/reconnecting-websocket.min.js', reconnecting_websocket)
     app.router.add_get('/ws', websocket_handler)
+    app['chat_members'] = {}
+    # each element is a tuple (time.time(), message)
+    app['messages_archive'] = []
     return app
+
+
+def dump_archival_messages_to_hard_drive(app):
+    chat_logger.info('Saving messages to file...')
+    data = json.dumps(app['messages_archive'])
+    with open('messages.dump', 'wb') as file:
+        file.write(data.encode())
+    chat_logger.info('Saved %d messages to file.', len(app['messages_archive']))
+    asyncio.get_event_loop().call_later(15, dump_archival_messages_to_hard_drive, app)
+
+
+def load_archival_messages(app):
+    try:
+        with open('messages.dump', 'rb') as file:
+            app['messages_archive'] = json.loads(file.read())
+        chat_logger.info('Loaded %d messages from file.', len(app['messages_archive']))
+    except FileNotFoundError:
+        chat_logger.warning('No messages loaded, because file does not exist.')
 
 
 if __name__ == '__main__':
     app = create_app()
+    load_archival_messages(app)
+    asyncio.get_event_loop().call_later(15, dump_archival_messages_to_hard_drive, app)
     web.run_app(app, port=8080)
--- projekt/tests.py
+++ projekt/tests.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from .app import create_app
 
 
@@ -5,9 +7,54 @@ async def test_receives_sent_message(test_client):
     client = await test_client(create_app)
     connection = await client.ws_connect('/ws?nickname=JohnDoe')
 
-    message = 'Hello, world!'
-    example_payload = {'message': message, 'from': 'JohnDoe', 'timestamp': 0}
-    await connection.send_json(example_payload)
-    response = await connection.receive_json()
+    await connection.send_json({
+        'message': 'Hello, world!', 'from': 'JohnDoe', 'timestamp': 0
+    })
+    response = await asyncio.wait_for(connection.receive_json(), 0.05)
 
-    assert response['message'] == message
+    assert response['message'] == 'Hello, world!'
+    assert response['from'] == 'JohnDoe'
+
+
+async def test_presence(test_client):
+    client = await test_client(create_app)
+
+    connection = await client.ws_connect('/ws?nickname=JohnDoe')
+
+    response = await client.get('/members')
+    members_list = await response.json()
+
+    assert members_list == ['JohnDoe']
+
+    await connection.close()
+    response = await client.get('/members')
+    members_list = await response.json()
+
+    assert members_list == []
+
+async def test_conversation_between_two_users(test_client):
+    client = await test_client(create_app)
+
+    connection_one = await client.ws_connect('/ws?nickname=JohnDoe')
+    connection_two = await client.ws_connect('/ws?nickname=JonSnow')
+
+    await connection_one.send_json({'message': 'xDDD', 'from': 'JohnDoe', 'timestamp': 0})
+    response = await asyncio.wait_for(connection_two.receive_json(), 0.05)
+
+    assert response['message'] == 'xDDD'
+    assert response['from'] == 'JohnDoe'
+
+
+async def test_new_client_gets_old_messages(test_client):
+    client = await test_client(create_app)
+
+    connection_one = await client.ws_connect('/ws?nickname=JohnDoe')
+
+    await connection_one.send_json({'message': 'LOL', 'from': 'JohnDoe', 'timestamp': 0})
+    await connection_one.close()
+
+    connection_two = await client.ws_connect('/ws?nickname=JonSnow')
+    response = await asyncio.wait_for(connection_two.receive_json(), 0.05)
+
+    assert response['message'] == 'LOL'
+    assert response['from'] == 'JohnDoe'
